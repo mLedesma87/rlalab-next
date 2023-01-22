15459e6cbe33d6fa3e0020860d14ab19ca796891
mLedesma87 <manuledesma87@gmail.com>
2023-01-22 21:12:45 +0100

    commit changes

--- pages/team.js
+++ pages/team.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Team.module.css';
 import {useEffect, useState} from "react";
+import {headers} from "next/headers";
 
 
 export default function Team() {
@@ -19,8 +20,8 @@ export default function Team() {
             <div className={styles.teamWrapper} data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" data-aos-offset="0">
                 {
                     !!members.length && members.map(({imgUrl, name, description}, index) => (
-                        <div key={index} style={{display:'flex', flexFlow:'column wrap', height:'auto', width: '450px'}}>
-                            <img src={imgUrl} style={{borderRadius:'50%', width:'80px'}} />
+                        <div key={index} style={{display:'flex', flexFlow:'column wrap', height:'auto', width: '450px', alignItems:'flex-start', justifyContent: 'flex-start'}}>
+                            <img src={imgUrl} style={{borderRadius:'60px', width:'120px', maxHeight: '120px', flex: '1'}} />
                             <div style={{display:'flex', flexFlow:'column wrap', fontSize:'1.3em', lineHeight:'33px'}}>
                                 <h3>{name}</h3>
                                 <span>{description}</span>
